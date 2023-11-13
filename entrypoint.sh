2c0943fc72ba1bbc46c25260fbba7e4d6af816fe
sodinokibi <74340780+sodinokibi@users.noreply.github.com>
2023-11-13 05:05:49 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Start rclone serve in the background to serve the /home/user/post directory over HTTP on port 8080
-rclone serve http /home/user/post --addr :8080 &
+rclone serve http /home/user/post --addr :8081 &
 
 # Initialize PoST data using the environment variables
 postcli -provider $PROVIDER \
